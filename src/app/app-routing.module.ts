5d6f9bac4cc898eef093c1d2854e97d36d525722
Daniel DRM. Matthiesen <dana@pc-tec.local>
2020-08-06 08:32:14 +0200

    Forside v0.1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ForsideComponent } from './forside/forside.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ForsideComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
