19a77df6c88ad98f10438756116f2330fafe34e8
EGalvit <Galvit28@gmail.com>
2020-08-10 09:40:24 +0200

    Routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { ForsideComponent } from './forside/forside.component'
+
+import { ForsideComponent } from './forside/forside.component';
+import { StartsideComponent } from './startside/startside.component';
+import { ProfilComponent } from './profil/profil.component';
+import { BeskederComponent } from './beskeder/beskeder.component';
+
 
 const routes: Routes = [
-  { path: '', component: ForsideComponent}
+  { path: '', component: StartsideComponent},
+  { path: 'forside', component: ForsideComponent},
+  { path: 'profil', component: ProfilComponent},
+  { path: 'beskeder', component: BeskederComponent},
 ];
 
 @NgModule({
