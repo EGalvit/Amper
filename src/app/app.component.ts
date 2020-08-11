d68370bc1dce11f8d283630fc8bd3982921deaf4
EGalvit <Galvit28@gmail.com>
2020-08-11 09:56:41 +0200

    navbar changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Amper';
+  router: string;
+
+  constructor(router: Router) { 
+    router.events.subscribe((url:any) => this.router = router.url);
+}
 }
