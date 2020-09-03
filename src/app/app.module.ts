427a838664729df758e2f8d8b200204223c66820
EGalvit <Galvit28@gmail.com>
2020-09-03 12:55:18 +0200

    Input validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { ForsideComponent } from './forside/forside.component';
 import { StartsideComponent } from './startside/startside.component';
@@ -25,10 +24,10 @@ import { AuthenticationService } from "./helpers/authentication.service";
   ],
   imports: [
     FormsModule,
+    ReactiveFormsModule,
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule,
-    ReactiveFormsModule
+    HttpClientModule
   ],
   providers: [
     AuthenticationService
--- src/app/startside/startside.component.ts
+++ src/app/startside/startside.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { HttpService } from "../helpers/http.service";
 import { AuthenticationService } from "../helpers/authentication.service";
 import { Router } from '@angular/router';
-import { FormGroup,  FormBuilder,  Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { ConfirmedValidator } from "../helpers/password.validator";
 
 
 @Component({
@@ -11,21 +12,47 @@ import { FormGroup,  FormBuilder,  Validators } from '@angular/forms';
   styleUrls: ['./startside.component.scss'],
 })
 export class StartsideComponent implements OnInit {
-  usernameCreate: string;
-  passwordCreate: string;
-  usernameLogin: string;
-  passwordLogin: string;
 
+  loginForm: FormGroup;
+  loginUsername = new FormControl(null, [Validators.required, Validators.minLength(6)]);
+  loginPassword = new FormControl(null, [Validators.required, Validators.minLength(6)]);
 
-  constructor(private _http: HttpService, private AuthenticationService: AuthenticationService, private router: Router) {
+  signupForm: FormGroup
+  signupUsername = new FormControl(null, [Validators.required, Validators.minLength(6)]);
+  signupPassword = new FormControl(null, [Validators.required, Validators.minLength(6)]);
+  signupConFirmPassword = new FormControl(null, [Validators.required]);
+  
+  constructor(private _http: HttpService, private AuthenticationService: AuthenticationService, private router: Router, private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      loginUsername: this.loginUsername,
+      loginPassword: this.loginPassword
+    });
+    
+    this.signupForm = this.fb.group({
+      signupUsername: this.signupUsername,
+      signupPassword: this.signupPassword,
+      signupConFirmPassword: this.signupConFirmPassword,
+
+    },
+    { validator: ConfirmedValidator('signupPassword', 'signupConFirmPassword') });
+  }
+  
+  loginSubmit() {
+    const formValue = this.loginForm.value;
+    this._http.Login(formValue.loginUsername, formValue.loginPassword);
   }
 
-  CreateSubmit() {
-    this._http.CreateUser(this.usernameCreate, this.passwordCreate);
+  signupSubmit() {
+    const formValue = this.signupForm.value;
+    this._http.CreateUser(formValue.signupUsername, formValue.signupPassword);
+  }
+  
+  get f(){
+    return this.loginForm.controls;
   }
 
-  LoginSubmit() {
-    this._http.Login(this.usernameLogin, this.passwordLogin);
+  get s(){
+    return this.signupForm.controls;
   }
 
   ngOnInit(): void {
